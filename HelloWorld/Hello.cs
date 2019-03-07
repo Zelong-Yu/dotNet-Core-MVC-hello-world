7e61da45bd21dcfc3e6e2a09caae5588c702a61e
Zelong Yu <zelong.yu@outlook.com>
2019-03-07 17:33:14 -0500

    Add project files.

---
+++ HelloWorld/Hello.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelloWorld
+{
+    public class Hello : IHello
+    {
+        public Hello()
+        {
+            //throw new Exception();
+        }
+        public string sayHello()
+        {
+            return "Hello From Class";
+        }
+    }
+}
